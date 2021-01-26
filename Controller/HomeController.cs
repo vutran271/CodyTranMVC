d06d442760c08ae969c715278e2be1c0fda1c967
Vu Tran <vntran@gtcc.edu>
2021-01-25 21:39:10 -0500

    Add project files.

---
+++ Controller/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodyTranMVC.Controller
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        // GET: HomeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
